ef2400ee9feb2068e7ab4ea414753e8ff891bc79
Bryan <bpfahert@gmail.com>
2022-07-07 12:17:29 -0500

    Code etch-a-sketch and button to reset based on number of squares chosen
    Coded a function to make an etch-a-sketch grid based on the number of squares chosen. A button on the
    top of the screen allows the user to input the number of square per side and resets the etch-a-sketch.

--- etch.js
+++ etch.js
@@ -1,13 +1,34 @@
 
 const container = document.querySelector('.container');
+const btn = document.querySelector('#sqnum');
 
-
-for (let i = 0; i < 256; i++) {
-    const div = document.createElement('div');
-    div.classList.add('box');
-    div.textContent = `${i+1}`;
-    container.appendChild(div);
+function makeGrid(squareNum) {
+    if (squareNum <= 100) {
+        for (let i = 0; i < (squareNum * squareNum); i++) {
+            const square = document.createElement('div');
+            square.style.width = `${100 / squareNum}%`; 
+            square.classList.add('box');
+            square.addEventListener('mouseover', () => {
+                square.style.backgroundColor = 'white';
+            });
+            container.appendChild(square);
+        };
     }
+    else {
+        alert("Please choose 100 or below.");
+    }
+};
 
+btn.addEventListener('click', () => {
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+    const newGrid = Number(prompt("Enter the number of squares per side.")); 
+    if (isNaN(newGrid) == true || newGrid <=0) {
+        alert("Error! Please enter a number greater than 0!"); 
+    }
+    makeGrid(newGrid);
 
-//  div.textContent = `${i}`;
+});
+
+makeGrid(16);
